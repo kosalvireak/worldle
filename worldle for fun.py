eccdeda30ef613b17523bdf8b67c239d27d5558a
khoeunkosalvireak <virakvary@gmal.com>
2022-04-28 16:34:10 +0700

    5 attemps

--- worldle for fun.py
+++ worldle for fun.py
@@ -3,9 +3,8 @@ from random import choice
 word = ["hello","tiger","phone","table","apple","zebra","learn"]
 main_word = choice(word)
 print(main_word)
-print(main_word[2])
-keep_loop = True
-while keep_loop:
+keep_loop = 0
+while keep_loop < 6:
     input_word = input("Enter 5 charater words: ")
     while len(input_word) > 5 or len(input_word) < 5:
         print("you word is not 5 charaters")
@@ -17,6 +16,7 @@ while keep_loop:
             else :
                 print("no",char)
             i += 1
+    keep_loop += 1
     if input_word == main_word:
         break
     # end=input("want again?(y/n):")
