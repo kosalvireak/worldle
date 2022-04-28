34258673087720be56cc2fd63d010eeb7b820041
khoeunkosalvireak <virakvary@gmal.com>
2022-04-28 16:03:38 +0700

    loop run correctly

--- worldle for fun.py
+++ worldle for fun.py
@@ -3,11 +3,12 @@ from random import choice
 word = ["hello","tiger","phone","table","apple","zebra","learn"]
 main_word = choice(word)
 print(main_word)
+print(main_word[2])
 input_word = input("Enter 5 charater words: ")
 i = 0
 for char in input_word:
-    if char[i] == main_word[i]:
-        print("yes",[char])
+    if char == main_word[i]:
+        print("yes",char)
     else :
-        print("no",[char])
+        print("no",char)
     i += 1
